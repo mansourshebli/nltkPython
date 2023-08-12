f38ba5d970f6d6df096fe2645a5f904c0c9a2b4d
Mansour Alshebli <132643497+mansourshebli@users.noreply.github.com>
2023-08-12 16:21:41 +0400

    final Update Named_Entity_Recognition.py

--- Named_Entity_Recognition.py
+++ Named_Entity_Recognition.py
@@ -1,1 +1,19 @@
 # @mansourshebli
+import nltk
+from nltk import ne_chunk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+# Sample text for named entity recognition
+sample_text = "Barack Obama was the 44th President of the United States. He was born in Hawaii."
+
+# Tokenize the sample text into words
+words = word_tokenize(sample_text)
+
+# Perform named entity recognition
+ner_result = ne_chunk(nltk.pos_tag(words))
+
+# Print the named entity recognition result
+print(ner_result)
