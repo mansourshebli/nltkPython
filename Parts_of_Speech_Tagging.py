cdcaf84abdc35b5938336617369bb5401ee7d103
Mansour Alshebli <132643497+mansourshebli@users.noreply.github.com>
2023-08-12 16:18:38 +0400

    final Update Parts_of_Speech_Tagging.py

--- Parts_of_Speech_Tagging.py
+++ Parts_of_Speech_Tagging.py
@@ -1,1 +1,21 @@
-# @mansourshebli
+ # @mansourshebli 
+
+# Tokenization with NLTK
+# Imported the required libraries
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+# Sample text for POS tagging demonstration
+sample_text = "I am learning how to use POS tagging with NLTK."
+
+# Tokenize the sample text into words
+tokens = word_tokenize(sample_text)
+
+# Perform POS tagging
+pos_tags = nltk.pos_tag(tokens)
+
+# Print the result
+for word, pos_tag in pos_tags:
+    print(f"Word: {word}, POS Tag: {pos_tag}")
